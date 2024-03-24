d8cf495fafe252111bb0f083c135843fc1fafca0
yohei0807 <157967778+yohei0807@users.noreply.github.com>
2024-03-24 11:15:54 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,1 +1,3 @@
-console.log(new Date().toString());
+const date = new Date();
+const month = date.getMonth() + 1;
+console.log(date.getFullYear().toString() + '年' + month.toString() + '月' + date.getDate().toString() + '日');
